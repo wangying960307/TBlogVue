4a6dcb5609310567c1259de3237d84f79a118290
gtguizhou <2990679726@qq.com>
2019-02-09 00:39:03 +0800

    将文章项，单独分离出来

--- src/pages/components/BlogContent.vue
+++ src/pages/components/BlogContent.vue
@@ -1,33 +1,9 @@
 <template>
     <el-card>
         <div v-for="(item,key) in items" :key="key">
-            <el-card>
-                <h2>{{item.title}}</h2>
-                <el-row>
-                    <el-col :lg="4" >
-                        <img v-if="item.cover" :src="item.cover" width="150px">
-                    </el-col>
-                    <el-col :lg="18" >
-                        <span class="article-content"
-                              v-html="item.content">
-                        </span>
-                    </el-col>
-                </el-row>
-
-                <el-row >
-                    <el-col :lg="18" >
-                        <gray-small>创建时间{{item.update_time}}</gray-small>
-                    </el-col>
-                    <el-col :lg="6"  style="text-align: right">
-                        <el-tag type="warning" @click="onStar(item)">
-                            <i class="el-icon-star-on"></i>
-                            {{item.star_num}}个赞
-                        </el-tag>
-                        &nbsp;
-                        <el-button type="success" @click="$router.push('/article/view/' + item.id)">查看</el-button>
-                    </el-col>
-                </el-row>
-            </el-card>
+            <article-item
+                    :item="item"
+            ></article-item>
             <br>
             <el-pagination-plus
                     style="justify-content: center"
@@ -42,11 +18,11 @@
 <script>
     import { ArticleIndex, ArticleStar } from '../../api/page.article'
     import ElPaginationPlus from '../components/el-pagination-plus/index'
-    import GraySmall from '../components/gray-small/index'
+    import ArticleItem from './article-item/index'
 
     export default {
         name: 'BlogContent',
-        components: { GraySmall, ElPaginationPlus },
+        components: { ArticleItem, ElPaginationPlus },
         data () {
             return {
                 loading: false,
---
+++ src/pages/components/article-item/index.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <el-card>
+            <h2>{{item.title}}</h2>
+            <el-row>
+                <el-col :lg="4" >
+                    <img v-if="item.cover" :src="item.cover" width="150px">
+                </el-col>
+                <el-col :lg="18" >
+                        <span class="article-content"
+                              v-html="item.content">
+                        </span>
+                </el-col>
+            </el-row>
+
+            <el-row >
+                <el-col :lg="18" >
+                    <gray-small>创建时间{{item.update_time}}</gray-small>
+                </el-col>
+                <el-col :lg="6"  style="text-align: right">
+                    <el-tag type="warning" @click="onStar(item)">
+                        <i class="el-icon-star-on"></i>
+                        {{item.star_num}}个赞
+                    </el-tag>
+                    &nbsp;
+                    <el-button type="success" @click="$router.push('/article-item/view/' + item.id)">查看</el-button>
+                </el-col>
+            </el-row>
+        </el-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ArticleItem',
+        props: {
+            item: {
+                type: Object,
+                default: () => {
+                    return {
+                        title: '',
+                        star_num: 0,
+                        cover: '',
+                        content: ''
+                    }
+                }
+            }
+        },
+        data () {
+            return {}
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
